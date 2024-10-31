6cb383c800f3ccf74f717ffa652d7dd2562be84c
Fabricioggiacomelli <fabricioggiacomelli@gmail.com>
2024-10-30 23:14:31 -0300

    fim

--- main.js
+++ main.js
@@ -11,7 +11,21 @@ function p() {
     x.style.color = "lightblue";
 }
 
+function tocar() {
+    const audioElement = document.getElementById('audio');
+    audioElement.play();
+}
+
+function toggleClassButtons() {
+    const buttons = document.querySelectorAll('#bt1, #bt2, #bt3');
+    
+    buttons.forEach(button => {
+        button.classList.toggle('active');
+    });
+}
 
 document.querySelector('.botão1').onclick = Texto  
 document.querySelector(".botão2").onclick = img;
 document.querySelector(".botão3").onclick = p;
+document.querySelector(".botão4").onclick = tocar;
+document.querySelector('.alterabotao').addEventListener("click", toggleClassButtons);
